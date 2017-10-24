96bf98a6acab579db6615cad21fba49a490573dc
Lexi Lee <31965477+cs030123@users.noreply.github.com>
2017-10-24 15:38:26 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,64 +1,77 @@
+/**
+ * app.js
+* 本文件用于构建游戏面板上的敌人和玩家对象，并处理其中的逻辑
+* 游戏规则：
+* 1. 通过键盘的上下左右键控制玩家穿越不断出现敌人的通道，未触碰到任何敌人到达对岸则为获胜。
+* 2. 在任何时刻与敌人相遇则返回原地重新开始。
+*/
+
+//canvas面板每格高度和宽度，方便update对象
 var perHeight = 83;
 var perWidth = 101;
-// 这是我们的玩家要躲避的敌人 
-var Enemy = function(x, y, s) {
-    // 要应用到每个敌人的实例的变量写在这里
-    // 我们已经提供了一个来帮助你实现更多
 
-    // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
-    this.sprite = 'images/enemy-bug.png';
-    //起始位置
+/**
+* @description 父类对象，包含敌人和玩家的公共属性
+* @param {number} x 对象横坐标
+* @param {number} y 对象纵坐标
+* @param {number} sprite 图片对象
+*/
+var Actor = function(x, y，sprite) {
     this.x = x;
     this.y = y;
+    this.sprite = sprite;
+};
+
+//在屏幕上绘制对象
+Actor.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x * perWidth, this.y * perHeight);
+};
+
+// 敌人类
+var Enemy = function(x, y, s) {
+    Actor.call(this, x, y, 'images/enemy-bug.png');
     //移动倍速
     this.s = s;
 };
 
-// 此为游戏必须的函数，用来更新敌人的位置
-// 参数: dt ，表示时间间隙
+/**
+* @description 更新敌人的位置
+* @param {number} dt ，表示时间间隙，每次移动*dt，屏蔽不同电脑的时间差
+*/
 Enemy.prototype.update = function(dt) {
-    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
-    // 都是以同样的速度运行的
     this.x = this.x + dt * 0.3 * this.s;
     if (this.x > 5) {
         this.x = -1;
     }
-    this.render();
 };
 
-// 此为游戏必须的函数，用来在屏幕上画出敌人，
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x * perWidth, this.y * perHeight);
-};
-
-// 现在实现你自己的玩家类
-// 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
-var Player = function(x, y, s){
-    this.sprite = 'images/char-boy.png';
-    //起始位置
-    this.x = x;
-    this.y = y;
+// 玩家类
+var Player = function(x, y){
+    Actor.call(this, x, y, 'images/char-boy.png');
     //在最上一行的偏移
     this.d = 0;
-}
+};
 
+//暂未发挥作用
 Player.prototype.update = function(dt){
     
 };
 
 Player.prototype.render = function(){
+    //如果玩家成功到达目的地，则呈现不停跳动的动画
     if (this.y < 1) {
-        this.d = this.d + 0.03;
-        if (this.d >= 0.15) {
+        this.d = this.d + 1;
+        if (this.d > 10) {
             this.d = 0;
         }
+        ctx.drawImage(Resources.get(this.sprite), this.x * perWidth, this.y * perHeight - this.d);  
     } else {
         this.d = 0;
+        ctx.drawImage(Resources.get(this.sprite), this.x * perWidth, this.y * perHeight);  
     }
-    ctx.drawImage(Resources.get(this.sprite), this.x * perWidth, (this.y - this.d) * perHeight);    
 };
 
-Player.prototype.handleInput = function(turn){
+Player.prototype.handleInput = function(turn) {
     switch(turn) {
         case 'up':
             if (this.y > 0) {
@@ -96,39 +109,29 @@ function checkCollisions(){
         }
     });
     if (isConflicting) {
-        resetPlayer();
+        //重置玩家的位置
+        player.y = 5;
+        player.x = 2;
+        player.render();
     }
-}
+};
 
-function showSuccess(){
-
-}
-
-function inRange(x, x1, x2){
+//判断x是否处于(x1, x2)区间
+function inRange(x, x1, x2) {
     return x > x1 && x < x2;
-}
+};
 
-function resetPlayer(){
-    player.y = 4.8;
-    player.x = 2;
-    player.render();
-}
-
-// 现在实例化你的所有对象
-// 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
-// 把玩家对象放进一个叫 player 的变量里面
+// 实例化敌人序列和玩家
 var allEnemies = [];
-//allEnemies.push(new Enemy(1, 2.8, 16));
 allEnemies.push(new Enemy(0, 0.8, 8));
 allEnemies.push(new Enemy(-2, 0.8, 16));
 allEnemies.push(new Enemy(-1, 1.8, 10));
 allEnemies.push(new Enemy(-15, 1.8, 18));
 allEnemies.push(new Enemy(-2, 2.8, 10));
 allEnemies.push(new Enemy(-8, 2.8, 12));
-var player = new Player(2, 5, 10);
+var player = new Player(2, 5);
 
-// 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
-// 方法里面。你不需要再更改这段代码了。
+// 键盘点击监听事件
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
--- js/engine.js
+++ js/engine.js
@@ -140,8 +140,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png',
-        'images/Star.png'
+        'images/char-boy.png'
     ]);
     Resources.onReady(init);
 
